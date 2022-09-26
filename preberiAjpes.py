d58449a9cf8d56e1a40759bd33adb420ee7ba1b4
Baselj <42610159+Baselj@users.noreply.github.com>
2022-09-26 12:45:50 +0200

    Update preberiAjpes.py

--- preberiAjpes.py
+++ preberiAjpes.py
@@ -1,7 +1,4 @@
-﻿#pip install selenium
-#pip install PyPDF2
-
-from asyncio.windows_events import NULL
+﻿from asyncio.windows_events import NULL
 import datetime
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
@@ -27,15 +24,19 @@ from email.mime.multipart import MIMEMultipart
 from email import encoders
 import sys
 
+#pip install selenium
+#pip install PyPDF2
+#gmail configuration
+#your gmail address 
+mailUname="" 
+#your gmail password (this is password generated in google after you complete two step verification)
+mailPwd=""
+#send from which email address
+fromEmail=""
+
 osnovniFolder =os.path.dirname(os.path.realpath(__file__))
 downloadFolder = osnovniFolder+r"\DownloadFolder"
 chromeDriverFolder = osnovniFolder+ r"\SeleniumDrivers\chromedriver.exe"
-#konfiguracija gmail
-
-mailUname=""
-mailPwd=""
-fromEmail=""
-
 kolikoVrsticZelim = "500"
 mailBody=""
 randomZakasnitev = random.randint(1,3)
